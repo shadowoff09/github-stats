ea535433f7c6b2812fd49d408df694d0aa987670
Diogo Gaspar <diogogaspar931@gmail.com>
2024-11-05 13:31:10 +0000

    add generate json

--- generate_images.py
+++ generate_images.py
@@ -29,43 +29,74 @@ def generate_output_folder() -> None:
 
 async def generate_overview(s: Stats) -> None:
     """
-    Generate an SVG badge with summary statistics
+    Generate a JSON file with summary statistics and an SVG badge
     :param s: Represents user's GitHub statistics
     """
+    # Gather data for JSON
+    overview_data = {
+        "name": await s.name,
+        "stars": await s.stargazers,
+        "forks": await s.forks,
+        "contributions": await s.total_contributions,
+        "lines_changed": (await s.lines_changed)[0] + (await s.lines_changed)[1],
+        "views": await s.views,
+        "repos": len(await s.repos)
+    }
+
+    # Write JSON file
+    generate_output_folder()
+    with open("generated/overview.json", "w") as f:
+        json.dump(overview_data, f, indent=4)
+
+    # Generate SVG
     with open("templates/overview.svg", "r") as f:
         output = f.read()
 
-    output = re.sub("{{ name }}", await s.name, output)
-    output = re.sub("{{ stars }}", f"{await s.stargazers:,}", output)
-    output = re.sub("{{ forks }}", f"{await s.forks:,}", output)
-    output = re.sub("{{ contributions }}", f"{await s.total_contributions:,}", output)
-    changed = (await s.lines_changed)[0] + (await s.lines_changed)[1]
-    output = re.sub("{{ lines_changed }}", f"{changed:,}", output)
-    output = re.sub("{{ views }}", f"{await s.views:,}", output)
-    output = re.sub("{{ repos }}", f"{len(await s.repos):,}", output)
+    output = re.sub("{{ name }}", overview_data["name"], output)
+    output = re.sub("{{ stars }}", f"{overview_data['stars']:,}", output)
+    output = re.sub("{{ forks }}", f"{overview_data['forks']:,}", output)
+    output = re.sub("{{ contributions }}", f"{overview_data['contributions']:,}", output)
+    output = re.sub("{{ lines_changed }}", f"{overview_data['lines_changed']:,}", output)
+    output = re.sub("{{ views }}", f"{overview_data['views']:,}", output)
+    output = re.sub("{{ repos }}", f"{overview_data['repos']:,}", output)
 
-    generate_output_folder()
     with open("generated/overview.svg", "w") as f:
         f.write(output)
 
 
 async def generate_languages(s: Stats) -> None:
     """
-    Generate an SVG badge with summary languages used
+    Generate a JSON file with summary languages used and an SVG badge
     :param s: Represents user's GitHub statistics
     """
+    # Gather and sort language data for JSON
+    sorted_languages = sorted(
+        (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size")
+    )
+
+    languages_data = {
+        lang: {
+            "size": data.get("size"),
+            "color": data.get("color"),
+            "percentage": data.get("prop", 0)
+        }
+        for lang, data in sorted_languages
+    }
+
+    # Write JSON file
+    generate_output_folder()
+    with open("generated/languages.json", "w") as f:
+        json.dump(languages_data, f, indent=4)
+
+    # Generate SVG
     with open("templates/languages.svg", "r") as f:
         output = f.read()
 
     progress = ""
     lang_list = ""
-    sorted_languages = sorted(
-        (await s.languages).items(), reverse=True, key=lambda t: t[1].get("size")
-    )
     delay_between = 150
     for i, (lang, data) in enumerate(sorted_languages):
-        color = data.get("color")
-        color = color if color is not None else "#000000"
+        color = data.get("color", "#000000")
         progress += (
             f'<span style="background-color: {color};'
             f'width: {data.get("prop", 0):0.3f}%;" '
@@ -79,13 +110,11 @@ fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8z"></path></svg>
 <span class="lang">{lang}</span>
 <span class="percent">{data.get("prop", 0):0.2f}%</span>
 </li>
-
 """
 
     output = re.sub(r"{{ progress }}", progress, output)
     output = re.sub(r"{{ lang_list }}", lang_list, output)
 
-    generate_output_folder()
     with open("generated/languages.svg", "w") as f:
         f.write(output)
 
