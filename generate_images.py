f32501af265c1e3c179a633785b8c54ac32a7fed
Diogo Gaspar <diogogaspar931@gmail.com>
2024-11-08 17:09:46 +0000

    reverted changes

--- generate_images.py
+++ generate_images.py
@@ -145,7 +145,7 @@ async def main() -> None:
         {x.strip() for x in exclude_langs.split(",")} if exclude_langs else None
     )
     # Convert a truthy value to a Boolean
-    raw_ignore_forked_repos = TRUE
+    raw_ignore_forked_repos = os.getenv("EXCLUDE_FORKED_REPOS")
     ignore_forked_repos = (
         not not raw_ignore_forked_repos
         and raw_ignore_forked_repos.strip().lower() != "false"
